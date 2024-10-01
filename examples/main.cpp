e4618710484700d8ff6126724ddd3a0063097bbb
Jason Stewart <jason00stewart@gmail.com>
2024-10-01 13:44:36 -0600

    Added starter files for template

---
+++ examples/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+#include <Cabana_Core.hpp>
+#include <Kokkos_Core.hpp>
+#include <NuMesh_Core.hpp>
+
+int main()
+{
+	printf("Hi\n");
+	return 0;
+}
